886e77b619eecedf3457fcd68abd47088df60007
f4110v3r <111393058+f4110v3r@users.noreply.github.com>
2025-09-20 04:13:00 -0800

    Map

--- frontend/App.js
+++ frontend/App.js
@@ -1,8 +1,8 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { Login } from './Login';
-import { StyleSheet, Text, View } from 'react-native';
-import { Dashboard } from './Dashboard'
+import { View, Text, StyleSheet } from 'react-native';
+import { MapScreen } from './MapScreen'
 import { SignIn } from './SignIn'
 
 const Stack = createNativeStackNavigator();
@@ -12,7 +12,7 @@ export default function App() {
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Login">
         <Stack.Screen name="Login" component={Login} />
-        <Stack.Screen name="Dashboard" component={Dashboard} />
+        <Stack.Screen name="MapScreen" component={MapScreen} />
         <Stack.Screen name="SignIn" component={SignIn} />
       </Stack.Navigator>
     </NavigationContainer>
